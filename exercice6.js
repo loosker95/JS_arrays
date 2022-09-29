029b0467ca24326495cab707c7d9859ddbd5f79c
Fabien <loosker95@gmail.com>
2022-09-29 11:12:57 +0700

    update exercice 6

--- exercice6.js
+++ exercice6.js
@@ -1,9 +1,9 @@
 
 const myArr = ['face', 'zalo', 'face', 'gmail', 'zalo', 'zalo'];
+const result = {};
 
+for (const content of myArr) {
+  (result[content]) ? result[content] += 1 : result[content] = 1;
+}
 
-const theOccurrences = myArr.reduce((value, items) =>{
-    return value[items] ? ++value[items] : value[items] = 1, value;
-  }, {});
-  
-  console.log(theOccurrences) 
+console.log(result);
