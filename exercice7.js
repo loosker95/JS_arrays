8656caaa4c7bb113b8c8c4c38aeb2722334386a6
Fabien <loosker95@gmail.com>
2022-10-03 17:25:50 +0700

    Add number 9

--- exercice7.js
+++ exercice7.js
@@ -1,24 +1,24 @@
 
-function car() {
-    const carBrand = [
-        { make: 'audi', model: 'r8', year: '2012' }, 
-        { make: 'audi', model: 'rs5', year: '2013' }, 
-        { make: 'ford', model: 'mustang', year: '2012' }, 
-        { make: 'ford', model: 'fusion', year: '2015' }, 
-        { make: 'kia', model: 'optima', year: '2012' }
-    ]
-    
 
-    return ([
-                [carBrand[0], carBrand[1]], 
-                [carBrand[2], carBrand[3]], 
-                [carBrand[4]]
-            ]       
-    );
+const arr1 = [
+    { make: 'audi', model: 'r8', year: '2012' }, 
+    { make: 'audi', model: 'rs5', year: '2013' }, 
+    { make: 'ford', model: 'mustang', year: '2012' }, 
+    { make: 'ford', model: 'fusion', year: '2015' }, 
+    { make: 'kia', model: 'optima', year: '2012' }
+]
+
+function getObject(name, arr) {
+    const result = arr.filter((item) => {
+        if(item.make === name){
+            return `${item.make}`;
+        }
+    });
+    // return  `{"${name}" : ${result.toString()}}`;
+    return  result;
 }
 
-
-console.log(car());
+console.log(getObject('kia', arr1));
 
 
      
